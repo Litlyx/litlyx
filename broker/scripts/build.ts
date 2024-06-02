89c5272504b3b026a27002a62e650ba450f20c23
antonio <anto.verdi27@gmail.com>
2024-06-02 16:26:26 +0200

    fix array exception on files broker

--- broker/scripts/build.ts
+++ broker/scripts/build.ts
@@ -8,7 +8,7 @@ const filesList = globSync('dist/**/*.js');
 filesList.forEach(file => {
     let raw = fs.readFileSync(file, 'utf8');
     for (const path in paths) {
-        const deep = (file.match(/\\/g) || []).length;
+        const deep = (file.match(/\\|\//g) || []).length;
         const pathText = path.replace('*', '');
         const toReplaceText = new RegExp(`"${pathText}(.*?)"`, 'g');
         raw = raw.replace(toReplaceText, `"${new Array(deep - 2).fill('../').join('')}${paths[path][0].replace('*', '')}${'$1'}"`);
