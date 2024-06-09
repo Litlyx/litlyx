4ea7d7f56804babf734077d2667b9ee81fcd6fed
Emily <adharaultimasheireen@gmail.com>
2024-06-09 22:28:28 +0200

    fix ecosystem.config.example

--- broker/ecosystem.config.example.cjs
+++ broker/ecosystem.config.example.cjs
@@ -1,12 +1,17 @@
 module.exports = {
     apps: [
         {
-            name: 'Producer',
-            port: '3000',
+            name: 'QueueBroker',
+            port: '3999',
             exec_mode: 'fork',
             script: './dist/producer/src/index.js',
             env: {
+                EMAIL_SERVICE: "",
+                EMAIL_HOST: "",
+                EMAIL_USER: "",
+                EMAIL_PASS: "",
                 PORT: "",
+                MONGO_CONNECTION_STRING: "",
                 REDIS_URL: "",
                 REDIS_USERNAME: "",
                 REDIS_PASSWORD: "",
--- producer/ecosystem.config.example.cjs
+++ producer/ecosystem.config.example.cjs
@@ -1,18 +1,13 @@
 module.exports = {
     apps: [
         {
-            name: 'QueueBroker',
+            name: 'Producer',
             port: '3000',
             exec_mode: 'cluster',
             instances: '2',
             script: './dist/broker/src/index.js',
             env: {
-                EMAIL_SERVICE: "",
-                EMAIL_HOST: "",
-                EMAIL_USER: "",
-                EMAIL_PASS: "",
                 PORT: "",
-                MONGO_CONNECTION_STRING: "",
                 REDIS_URL: "",
                 REDIS_USERNAME: "",
                 REDIS_PASSWORD: "",
