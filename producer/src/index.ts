1e8f6f78dba0920e05f87a2cf5e9b6b7201156d5
Emily <adharaultimasheireen@gmail.com>
2024-06-09 19:29:39 +0200

    fix keep_alive

--- producer/src/index.ts
+++ producer/src/index.ts
@@ -53,7 +53,10 @@ app.post('/keep_alive', express.json(jsonOptions), async (req, res) => {
     try {
         const ip = getIPFromRequest(req);
         const sessionHash = createSessionHash(req.body.website, ip, req.body.userAgent);
-        await RedisStreamService.addToStream(streamName, { ...req.body, _type: 'keep_alive', sessionHash, ip });
+        await RedisStreamService.addToStream(streamName, {
+            ...req.body, _type: 'keep_alive', sessionHash, ip,
+            instant: req.body.instant + ''
+        });
         return res.sendStatus(200);
     } catch (ex: any) {
         return res.status(500).json({ error: ex.message });
