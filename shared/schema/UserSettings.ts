da6984add934af754080803529c94ff550fc1924
Emily <adharaultimasheireen@gmail.com>
2024-06-11 18:24:52 +0200

    fix schema

--- shared/schema/UserSettings.ts
+++ shared/schema/UserSettings.ts
@@ -2,11 +2,13 @@ import { model, Schema, Types } from 'mongoose';
 
 export type TUserSettings = {
     user_id: Schema.Types.ObjectId,
+    max_projects: number,
     active_project_id: Schema.Types.ObjectId
 }
 
 const UserSettingsSchema = new Schema<TUserSettings>({
     user_id: { type: Types.ObjectId, unique: true, index: 1 },
+    max_projects: { type: Number, default: 3 },
     active_project_id: Schema.Types.ObjectId,
 });
 
