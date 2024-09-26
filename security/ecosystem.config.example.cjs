f4401d74a2d3da2d0a2d370ce028efbc0d8190a9
Emily <adharaultimasheireen@gmail.com>
2024-09-26 15:53:00 +0200

    fix ecosystem

--- security/ecosystem.config.example.cjs
+++ security/ecosystem.config.example.cjs
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'security',
             exec_mode: 'fork',
-            script: './.output/security/index.mjs',
+            script: './.output/security/src/index.mjs',
             env: {
                 MONGO_CONNECTION_STRING: "",
                 EMAIL_SERVICE: "",
