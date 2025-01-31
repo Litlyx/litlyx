e9312355332bc20eccef64f48ea90891683a3f53
Emily <adharaultimasheireen@gmail.com>
2025-01-31 18:10:22 +0100

    removed shared

--- consumer/src/shared/utils/requireEnv.ts
+++
@@ -1,9 +0,0 @@
-
-export function requireEnv(name: string, errorMessage?: string) {
-    if (!process.env[name]) {
-        console.error(errorMessage || `ENV variable ${name} is required`);
-        return process.exit(1);
-    }
-    console.log('requireEnv', name, process.env[name]);
-    return process.env[name] as string;
-}
