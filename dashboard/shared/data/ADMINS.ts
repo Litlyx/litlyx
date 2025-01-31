a1718875d9d8d10e91db344ee2defbc1abe09a48
Emily <adharaultimasheireen@gmail.com>
2025-01-31 18:46:13 +0100

    remove shared

--- dashboard/shared/data/ADMINS.ts
+++
@@ -1,5 +0,0 @@
-export const ADMIN_EMAILS = [
-    'laura.emily.lovi@gmail.com',
-    'mangaiomaster@gmail.com',
-    'helplitlyx@gmail.com'
-]
--- dashboard/shared/data/broker/Limits.ts
+++
@@ -1,5 +0,0 @@
-
-
-// Default: 1.01
-// ((events + visits) * VALUE) > limit
-export const MAX_LOG_LIMIT_PERCENT = 1.01;
